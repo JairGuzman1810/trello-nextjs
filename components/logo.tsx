d9f73886797bf75ddfa8f676b855372a79eacf2f
JairGuzman1810 <jair18101@hotmail.com>
2024-09-25 16:45:43 -0600

    marketing navbar

--- components/logo.tsx
+++ components/logo.tsx
@@ -10,11 +10,9 @@ const headingFont = localFont({
 function Logo() {
   return (
     <Link href={"/"}>
-      <div className="hover-opacity-75 transition items-center gap-x-2 hidden md:flex">
-        <Image src={"../app/logo.svg"} alt="logo" height={30} width={30} />
-        <p
-          className={cn("text-lg text-neutral-700 pb-1", headingFont.className)}
-        >
+      <div className="hover:opacity-75 transition items-center gap-x-2 hidden md:flex">
+        <Image src={"/logo.svg"} alt="logo" height={30} width={30} />
+        <p className={cn("text-lg text-neutral-700 ", headingFont.className)}>
           Taskifiy
         </p>
       </div>
