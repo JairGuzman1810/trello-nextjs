0decc8ca67f7501921b40a1dfc639b46a3ee87b9
JairGuzman1810 <jair18101@hotmail.com>
2024-09-26 18:47:39 -0600

    organizations settings

---
+++ app/(platform)/(dashboard)/organization/[organizationId]/settings/page.tsx
@@ -0,0 +1,26 @@
+import { OrganizationProfile } from "@clerk/nextjs";
+
+const SettingsPage = () => {
+  return (
+    <div className="w-full">
+      <OrganizationProfile
+        appearance={{
+          elements: {
+            rootBox: {
+              boxShadow: "none",
+              width: "100%",
+              marginBottom: 20,
+            },
+            cardBox: {
+              border: "1px solid #e5e5e5",
+              boxShadow: "none",
+              width: "100%",
+            },
+          },
+        }}
+      />
+    </div>
+  );
+};
+
+export default SettingsPage;
