fc077672697b61b7120e5b853c2eb1f79822c4b5
JairGuzman1810 <jair18101@hotmail.com>
2024-09-30 13:41:25 -0600

    errors handling

--- app/(platform)/(dashboard)/organization/[organizationId]/form.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/form.tsx
@@ -8,13 +8,24 @@ export const Form = () => {
   const [state, dispatch] = useFormState(create, initialState);
   return (
     <form action={dispatch}>
-      <input
-        id="title"
-        name="title"
-        required
-        placeholder="Enter a board title"
-        className="border-black border p-1"
-      />
+      <div className="flex flex-col space-y-2">
+        <input
+          id="title"
+          name="title"
+          required
+          placeholder="Enter a board title"
+          className="border-black border p-1"
+        />
+        {state?.errors?.title ? (
+          <div>
+            {state.errors.title.map((error: string) => (
+              <p key={error} className="text-rose-500">
+                {error}
+              </p>
+            ))}
+          </div>
+        ) : null}
+      </div>
       <Button type="submit">Submit</Button>
     </form>
   );
