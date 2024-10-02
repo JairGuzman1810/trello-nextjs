1882d3cc9d2df4778e92401fa5da2bee1e4cb994
JairGuzman1810 <jair18101@hotmail.com>
2024-10-02 11:53:04 -0600

    unsplash image form picker wip

--- components/form/form-popover.tsx
+++ components/form/form-popover.tsx
@@ -8,6 +8,7 @@ import { toast } from "sonner";
 import { Button } from "../ui/button";
 import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
 import { FormInput } from "./form-input";
+import { FormPicker } from "./form-picker";
 import { FormSubmit } from "./form-submit";
 
 interface FormPopoverProps {
@@ -60,6 +61,7 @@ export const FormPopover = ({
         </PopoverClose>
         <form action={onSubmit} className="space-y-4">
           <div className="space-y-4">
+            <FormPicker id="image" errors={fieldErrors} />
             <FormInput
               id="title"
               label="Board title"
