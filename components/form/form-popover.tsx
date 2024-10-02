6b013f4b21455e6fc5c797f6a44f5523f1367a99
JairGuzman1810 <jair18101@hotmail.com>
2024-10-02 10:06:11 -0600

    added popover form popover wip board list wip

---
+++ components/form/form-popover.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
+
+interface FormPopoverProps {
+  children: React.ReactNode;
+  side?: "left" | "right" | "top" | "bottom";
+  align?: "start" | "center" | "end";
+  sideOffset?: number;
+}
+export const FormPopover = ({
+  children,
+  side = "bottom",
+  align,
+  sideOffset = 0,
+}: FormPopoverProps) => {
+  return (
+    <Popover>
+      <PopoverTrigger asChild>{children}</PopoverTrigger>
+      <PopoverContent
+        align={align}
+        className="w-80 pt-3"
+        side={side}
+        sideOffset={sideOffset}
+      >
+        <div className="text-sm font-medium text-center text-neutral-600 pb-4">
+          Create board
+        </div>
+      </PopoverContent>
+    </Popover>
+  );
+};
