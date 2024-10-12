168dab36a7552bbc97f1989b66648f931154f105
JairGuzman1810 <jair18101@hotmail.com>
2024-10-12 10:25:04 -0600

    card form wip form text area component

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-item.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-item.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { ListWithCards } from "@/types";
+import { ElementRef, useRef, useState } from "react";
+import { CardForm } from "./card-form";
 import { ListHeader } from "./list-header";
 
 interface ListItemProps {
@@ -8,10 +10,31 @@ interface ListItemProps {
   data: ListWithCards;
 }
 export const ListItem = ({ index, data }: ListItemProps) => {
+  const textareaRef = useRef<ElementRef<"textarea">>(null);
+  const [isEditing, setIsEditing] = useState(false);
+
+  const enableEditing = () => {
+    setIsEditing(true);
+    setTimeout(() => {
+      textareaRef.current?.focus();
+    });
+  };
+
+  const disableEditing = () => {
+    setIsEditing(false);
+  };
+
   return (
     <li className="shrink-0 h-full w-[272px] select-none">
       <div className="w-full rounded-md bg-[#f1f2f4] shadow-md pb-2">
-        <ListHeader data={data} />
+        <ListHeader onAddCard={enableEditing} data={data} />
+        <CardForm
+          ref={textareaRef}
+          listId={data.id}
+          isEditing={isEditing}
+          enableEditing={enableEditing}
+          disableEditing={disableEditing}
+        />
       </div>
     </li>
   );
